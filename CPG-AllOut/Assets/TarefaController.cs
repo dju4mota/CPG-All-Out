a1236f3c15f66365b33ab0bfdc7300a52cd5b18f
Felipe Conrado <fconrado2003@gmail.com>
2025-05-17 02:10:01 -0300

    add dinheiro

--- CPG-AllOut/Assets/TarefaController.cs
+++ CPG-AllOut/Assets/TarefaController.cs
@@ -1,16 +1,33 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TarefaController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Image bgImage;
+    public Image circleImage; 
+    public float fillSpeed = 0.1f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        circleImage.fillAmount += fillSpeed * Time.deltaTime;
+        circleImage.fillAmount = Mathf.Clamp01(circleImage.fillAmount);
+
+        if(circleImage.fillAmount == 1){
+            StartCoroutine(Fail());
+        }
+    }
+
+    void Complete(){
+
+    }
+
+    IEnumerator Fail(){
+        bgImage.color = Color.red;
+        yield return new WaitForSeconds(0.25f);
+        Destroy(gameObject);
+
     }
 }
