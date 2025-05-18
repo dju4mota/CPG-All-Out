030926a33e263a70f43e26947f8749e5b95eda83
MarceloHOPV <marcelo.henrique@gec.inatel.br>
2025-05-18 02:23:48 -0300

    cena final

--- CPG-AllOut/Assets/scripts/End.cs
+++ CPG-AllOut/Assets/scripts/End.cs
@@ -11,7 +11,10 @@ public class End : MonoBehaviour
     public TMP_Text pontos;
     public TMP_Text dinheiro;
     public TMP_Text tarefas;
+    public TMP_Text erros;
     public TMP_Text total;
+    //Game manager pra pegar as tarefas feitas
+
 
 
     private void Start()
@@ -33,27 +36,38 @@ public class End : MonoBehaviour
 
     IEnumerator MostrarPontos()
     {
-        yield return new WaitForSecondsRealtime(1f);
-        pontos.text = "pontos: " + GameData.i.pontos;
+        yield return new WaitForSecondsRealtime(0.5f);
+        pontos.text = (GameData.i.pontos).ToString();
         StartCoroutine("MostrarDinheiro");
     }
 
     IEnumerator MostrarDinheiro()
     {
-        yield return new WaitForSecondsRealtime(1f);
-        dinheiro.text = "dinheiro: " + GameData.i.dinheiros;
+        yield return new WaitForSecondsRealtime(0.5f);
+        dinheiro.text = (GameData.i.dinheiros).ToString();
         StartCoroutine("MostrarTarefas");
     }
     IEnumerator MostrarTarefas()
     {
-        yield return new WaitForSecondsRealtime(1f);
-        tarefas.text = "tarefas: " + GameData.i.tarefasFeitas;
+        yield return new WaitForSecondsRealtime(0.5f);
+        tarefas.text = (GameData.i.tarefasFeitas).ToString();
+        StartCoroutine("MostrarErros");
+    }
+
+    IEnumerator MostrarErros()
+    {
+        yield return new WaitForSecondsRealtime(0.5f);
+        int errosRestantes = Mathf.Max(0, GameData.i.totalTasks - GameData.i.tarefasFeitas);
+        erros.text = errosRestantes.ToString();
         StartCoroutine("MostrarTotal");
     }
+
+
+
     IEnumerator MostrarTotal()
     {
-        yield return new WaitForSecondsRealtime(1f);
-        total.text = "total: " +( (int) GameData.i.tarefasFeitas + (int) GameData.i.dinheiros + (int) GameData.i.pontos);
+        yield return new WaitForSecondsRealtime(0.5f);
+        total.text = "Total: " +( (int) GameData.i.tarefasFeitas + (int) GameData.i.dinheiros + (int) GameData.i.pontos);
     }
 
 }
